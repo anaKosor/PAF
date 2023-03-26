ac522e527ca9690ab6af19be0f0b49a88bf5e5e5
Ana Kosor <akosor@pmfst.hr>
2023-03-26 11:46:46 +0100

    zadatak 4

--- Vjezbe/vjezbe_3/linregress.py
+++ Vjezbe/vjezbe_3/linregress.py
@@ -6,17 +6,16 @@ import statistics
 M = [0.052, 0.124, 0.168, 0.236, 0.284, 0.336] # y vrijednost
 φ= [0.1745, 0.3491, 0.5236, 0.6981, 0.8727, 1.0472] # x vrijednost
 
+def mean_xy(lista1,lista2,len):
+    lista_xy=[]
+    i=0
+    while i<len:
+        xy=lista1[i]*lista2[i]
+        lista_xy.append(xy)
+        i=i+1
+    return statistics.mean(lista_xy)
+mean__xy=mean_xy(M,φ,len(M))
 
-# treba napraviti prvo liste umnoska x,y pa nihovu prosjecnu vrijednost
-#napraviti listu x^2 i njigovu prosječnu vrijednost za koficijent a
-i=0
-lista_xy=[]
-while i<(len(φ)):
-    xy=φ[i]*M[i]
-    lista_xy.append(xy)
-    i=i+1
-mean_xy=statistics.mean(lista_xy)
-print(mean_xy)
 def mean_kvadrat(lista,len_liste):#funkcija za izračunavanje srednje vrijednosti liste (xˇ2)
     i=0
     lista_i=[]
@@ -25,12 +24,15 @@ def mean_kvadrat(lista,len_liste):#funkcija za izračunavanje srednje vrijednost
         lista_i.append(x2)
         i=i+1
     return statistics.mean(lista_i)
-mean_xˇ2=mean_kvadrat(φ,len(φ))
 
-a=mean_xy/mean_xˇ2 # koficijent a
+mean_xˇ2=mean_kvadrat(φ,len(φ)) #sredja vrijednost liste xˇ2
 
-mean_yˇ2=mean_kvadrat(M,len(M))
+a=mean__xy/mean_xˇ2 # koficijent a
+
+mean_yˇ2=mean_kvadrat(M,len(M)) #srednja vrijednost liste yˇ2(tj.M)
+
 greska=math.sqrt(1/len(M)*((mean_yˇ2/mean_xˇ2)-a*a))
-
-print(mean_kvadrat(φ,len(φ)))
-print(lista_xy,mean_xy,mean_xˇ2,a)
+os_y=a*np.array([0.1745, 0.3491, 0.5236, 0.6981, 0.8727, 1.0472])
+plt.scatter(φ,M,color = 'green')
+plt.plot(φ,os_y,color='red')
+plt.show()
