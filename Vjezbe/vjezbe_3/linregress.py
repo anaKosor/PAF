f87f4d68a6b9c0e29fc5aa674326a899d2633398
Ana Kosor <akosor@pmfst.hr>
2023-03-30 18:38:27 +0100

    zadatak 4

--- Vjezbe/vjezbe_3/linregress.py
+++ Vjezbe/vjezbe_3/linregress.py
@@ -2,11 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import statistics
-
-M = [0.052, 0.124, 0.168, 0.236, 0.284, 0.336] # y vrijednost
+M = [0.052, 0.124, 0.168, 0.236, 0.284, 0.336] #y vrijednost izmejerenog
 φ= [0.1745, 0.3491, 0.5236, 0.6981, 0.8727, 1.0472] # x vrijednost
 
-def mean_xy(lista1,lista2,len):
+def mean_xy(lista1,lista2,len):#funkcija za izračunavanje srednje vrijednosti umnoška 2 liste 
     lista_xy=[]
     i=0
     while i<len:
@@ -27,12 +26,18 @@ def mean_kvadrat(lista,len_liste):#funkcija za izračunavanje srednje vrijednost
 
 mean_xˇ2=mean_kvadrat(φ,len(φ)) #sredja vrijednost liste xˇ2
 
-a=mean__xy/mean_xˇ2 # koficijent a
+a=mean__xy/mean_xˇ2 # a ili po formuli Dt
 
-mean_yˇ2=mean_kvadrat(M,len(M)) #srednja vrijednost liste yˇ2(tj.M)
+mean_yˇ2=mean_kvadrat(M,len(M)) #srednja vrijednost liste yˇ2(tj.M'zadano')
 
-greska=math.sqrt(1/len(M)*((mean_yˇ2/mean_xˇ2)-a*a))
-os_y=a*np.array([0.1745, 0.3491, 0.5236, 0.6981, 0.8727, 1.0472])
+deviacija=math.sqrt(1/len(M)*((mean_yˇ2/mean_xˇ2)-a*a))
+os_y=[]
+q=0
+while q<(len(φ)): #možemo napraviti lisu umnpška a(Dt)i phi da dobijemo M(tj. os y),ili os_y=a*np.array([0.1745, 0.3491, 0.5236, 0.6981, 0.8727, 1.0472])
+    e=a*φ[q]
+    os_y.append(e)
+    q=q+1
+
 plt.scatter(φ,M,color = 'green')
 plt.plot(φ,os_y,color='red')
 plt.show()
