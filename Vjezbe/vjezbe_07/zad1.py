63700cbe32286de3d1e3746df3409b9551408ccd
Ana Kosor <akosor@pmfst.hr>
2023-06-04 17:23:03 +0100

    zadatak

--- Vjezbe/vjezbe_07/zad1.py
+++ Vjezbe/vjezbe_07/zad1.py
@@ -13,7 +13,6 @@ x3,y3=x3.plot('Euler')
 plt.plot(x1,y1,label ='Euler dt=0.1')
 plt.plot(x3,y3,label ='Euler dt=0.05')
 plt.plot(x2,y2,label ='Euler dt=0.0001')
-
 plt.xlabel("x")
 plt.ylabel("y")
 plt.legend()
